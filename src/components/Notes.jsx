7276e5fc0643269bc60bb0bfa140242a8c4afbae
unknown <ankitbkana@outlook.com>
2024-02-04 23:42:57 +0530

    making editable

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 function Notes() {
+  const handling = (event) => {
+    console.log(event.target.innerHTML);
+  };
+
   return (
     <div
       contentEditable={true}
-      
+      onInput={handling}
       className=" text-sm leading-4 mt-3 bg-yellow-50 p-2 rounded-xl">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, earum! Et
       corrupti sequi quibusdam odit impedit quod eos, excepturi architecto quo
